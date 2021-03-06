817ded56f7a339c575f9d2c7c45f8720a6c48ecd
unknown <andypythonappdeveloper@gmail.com>
2021-03-06 11:53:08 +1100

    Add private checks

--- src/routes/public.rs
+++ src/routes/public.rs
@@ -4,8 +4,15 @@ use rocket::response::NamedFile;
 use rocket::*;
 
 #[get("/static/<file..>")]
-pub fn get(file: PathBuf) -> Option<NamedFile> {
+pub fn get(file: PathBuf) -> Result<NamedFile, String> {
+    if file.to_string_lossy().contains("private") {
+        return Err("Access Denied".into());
+    }
+
     let path = Path::new("static").join(file);
 
-    NamedFile::open(path).ok()
+    match NamedFile::open(path) {
+        Ok(file) => Ok(file),
+        Err(_) => Err("File not found.".into()),
+    }
 }
