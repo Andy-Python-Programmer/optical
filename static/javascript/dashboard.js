e6934c446132fb57b245cbab409995f73e54d15b
unknown <andypythonappdeveloper@gmail.com>
2021-03-07 14:02:46 +1100

    Country selector

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,37 +1,54 @@
 const GLOBE_IMAGE_TEXTURE = "/static/images/globe.png";
 
-let data;
+async function main() {
+    let data = await (await fetch("/static/geo.json")).json();
 
-function get_country_data(target_country) {
+    const canvas = document.getElementById("canvas");
+
+    let country_selector = document.getElementById("country-select");
+    let features_vector = {};
+
     for (let i = 0; i < data.features.length; i++) {
         const country = data.features[i].properties.NAME;
 
-        if (target_country == country) {
-            return [data.features[i]]
-        }
+        country_selector.innerHTML += `<option value="${country}">${country}</option>`;
+        features_vector[country] = [data.features[i]]
     }
-}
 
-async function main() {
-    data = await (await fetch("/static/geo.json")).json();
-
-    const main = document.getElementById("main");
-    const country_data = get_country_data("Australia");
-
-    let earth = Globe()(main)
+    let earth = Globe()(canvas)
         .hexPolygonResolution(3)
         .hexPolygonMargin(0.3)
         .hexPolygonLabel(({ properties: d }) => {
             return `<b>${d.NAME}</b>`;
         })
-        .hexPolygonsData(country_data)
         .hexPolygonColor(() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`)
         .globeImageUrl(GLOBE_IMAGE_TEXTURE);
 
-    earth.pointOfView({
-        "lng": country_data[0]["geometry"]["coordinates"][0][0][0][0],
-        "lat": country_data[0]["geometry"]["coordinates"][0][0][0][1],
+    earth.controls().autoRotate = true;
+    earth.controls().autoRotateSpeed = -0.3;
+
+    country_selector.addEventListener("change", (event) => {
+        const target_country = event.target.value;
+
+        let country_data = features_vector[target_country];
+        let cords = country_data[0]["geometry"]["coordinates"];
+
+        while (true) {
+            cords = cords[0];
+
+            if (typeof cords[0] == "number") {
+                break;
+            }
+        }
+
+        earth
+            .hexPolygonsData(country_data)
+            .pointOfView({
+                "lng": cords[0],
+                "lat": cords[1],
+            });
     });
+
 
     // Make the our earth responsive :D
     window.addEventListener('resize', (event) => {
