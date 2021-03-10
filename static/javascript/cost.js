56761e319b6552b93e46630a03ce0b3fa78f5de9
unknown <andypythonappdeveloper@gmail.com>
2021-03-10 19:43:36 +1100

    Cleanups

--- static/javascript/cost.js
+++ static/javascript/cost.js
@@ -1,43 +1,48 @@
+/// The zoom level for the map.
+const DEFAULT_MAP_ZOOM = 18;
+/// The default map style.
+const DEFAULT_MAP_STYLE = "mapbox://styles/mapbox/satellite-v9";
+/// Mapbox access token.
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiYW5keXB5dGhvbmFwcGRldmVsb3BlciIsImEiOiJja2x5cXZoejUwNHJoMm9xc2F1Z2cweXUzIn0.DQkJOVfq_JZz9u27SLbqBw";
+
 (
-    // Init.
-    async () => { }
+    async () => {
+        mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+    }
 )();
 
 async function main_res(area) {
     let panels_c = document.getElementById("panels-count");
 
-    console.log(`Roof Area: ${area}`);
-    console.log(`You can fit around ${Math.round(area / 1.7)} panels.`)
+    let panel_count = Math.round(area / 1.7);
 
-    panels_c.innerHTML = `${Math.round(area / 1.7)} solar panels.`;
+    console.log(`Roof Area: ${area}`);
+    console.log(`You can fit around ${panel_count} panels.`)
+
+    panels_c.innerHTML = `${panel_count} solar panels.`;
 }
 
 async function main(argv) {
-    // This is the public access token. You can only access it if you are using optical :)
-    mapboxgl.accessToken = "pk.eyJ1IjoiYW5keXB5dGhvbmFwcGRldmVsb3BlciIsImEiOiJja2x5cXZoejUwNHJoMm9xc2F1Z2cweXUzIn0.DQkJOVfq_JZz9u27SLbqBw";
+    let center = [argv.get("lat"), argv.get("lng")];
 
-    let map = (window.map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/satellite-v9",
-        center: [argv.get("lat"), argv.get("lng")],
-        zoom: 18,
-    }));
+    let map_config = {
+        "container": "map",
+        "center": center,
+        "style": DEFAULT_MAP_STYLE,
+        "zoom": DEFAULT_MAP_ZOOM,
+    };
 
-    let calc_next = document.getElementById("calc-next");
-    let answer = document.getElementById("calculated-area");
-
-
-    // Sections.
-    let calc = document.getElementById("calc");
-    let calc_res = document.getElementById("calc-res");
-
-    let draw = new MapboxDraw({
-        displayControlsDefault: false,
-        controls: {
-            polygon: true,
-            trash: true
+    let overlay_config = {
+        "displayControlsDefault": false,
+        "controls": {
+            "polygon": true,
+            "trash": true
         }
-    });
+    };
+
+    let map = window.map = new mapboxgl.Map(map_config);
+
+    let draw = new MapboxDraw(overlay_config);
 
     map.addControl(draw);
 
@@ -47,7 +52,13 @@ async function main(argv) {
 
     var area;
 
-    function updateArea() {
+    let calc_next = document.getElementById("calc-next");
+    let answer = document.getElementById("calculated-area");
+
+    let calc = document.getElementById("calc");
+    let calc_res = document.getElementById("calc-res");
+
+    let updateArea = () => {
         var data = draw.getAll();
 
         if (data.features.length > 0) {
@@ -55,12 +66,14 @@ async function main(argv) {
             const rounded_area = Math.round(area_t * 100) / 100;
 
             area = rounded_area;
-            answer.innerHTML = `<p>Your roof area is around: <strong>${rounded_area}</strong> square meters.`;
             calc_next.style.display = "initial";
+
+            answer.innerHTML = `<p>Your roof area is around: <strong>${rounded_area}</strong> square meters.`;
         } else {
             area = undefined;
-            answer.innerHTML = "";
             calc_next.style.display = "none";
+
+            answer.innerHTML = "";
         }
     }
 
@@ -72,5 +85,4 @@ async function main(argv) {
     });
 }
 
-let args = new URLSearchParams(window.location.search);
-main(args);
+main(new URLSearchParams(window.location.search));
